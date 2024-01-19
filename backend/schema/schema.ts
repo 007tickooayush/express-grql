2b46ad190b6672fa270b2a5bd7b342ce7c2574d9
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-01-19 16:58:12 +0530

    static data schema created

--- backend/schema/schema.ts
+++ backend/schema/schema.ts
@@ -1,8 +1,8 @@
 import { projects, clients } from '../sampleData';
-import { GraphQLID, GraphQLObjectType, GraphQLSchema, GraphQLString, } from 'graphql';
+import { GraphQLID, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString, } from 'graphql';
 
 
-export const ClientType = new GraphQLObjectType({
+const ClientType = new GraphQLObjectType({
     name: 'Client',
     fields: () => ({
         id: { type: GraphQLID },
@@ -12,9 +12,45 @@ export const ClientType = new GraphQLObjectType({
     })
 });
 
+
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client: { 
+            type: ClientType,
+            resolve(parent,args){
+                return clients.find((client) => client.id === parent.clientId)
+            }
+        }
+    })
+});
+
 export const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: () => ({
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve: (parent, args) => {
+                return projects.find(project => project.id === args.id);
+            }
+        },
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients;
+            }
+        },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },

c643bddae70611cfebe7bc56cfd1527f40d7948b
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-01-19 12:55:50 +0530

    backend graphql schema setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,20 @@
-const express = require('express')
-
+// const express = require('express')
+import express from 'express';
+import schema from './schema/schema'
 const app = express();
 
+import { graphqlHTTP } from 'express-graphql';
+import { GraphQLSchema } from 'graphql';
 
+require('dotenv').config();
 const PORT = process.env.PORT || 5000
+
+app.use('/graphql',graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
 
 
 app.listen(PORT, () => {
-    console.log('SERVER RUNNING ON PORT: ',PORT);
+    console.log('SERVER RUNNING ON PORT: ', PORT);
 })

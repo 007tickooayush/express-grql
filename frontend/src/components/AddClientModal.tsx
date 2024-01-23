bb530be8af91f5aaa2d66ad5320b2910aceac5db
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-01-23 11:05:19 +0530

    Client Creation modal + GraphQL mutation done

--- frontend/src/components/AddClientModal.tsx
+++ frontend/src/components/AddClientModal.tsx
@@ -1,13 +1,44 @@
 // import React from 'react';
 
+import { useMutation } from "@apollo/client";
 import { useState } from "react";
 import { FaUser } from "react-icons/fa";
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
 
 const AddClientModal = () => {
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
+
+    const [addClient] = useMutation(ADD_CLIENT, {
+        variables: { name, email, phone },
+        update(cache, { data: { addClient } }) {
+            const { clients }: any = cache.readQuery({
+                query: GET_CLIENTS
+            });
+
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                data: { clients: [...clients, addClient] }
+            });
+        }
+    })
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // console.log(name,email,phone);
+        if (name == '' || email == '' || phone == '') {
+            return alert('Please fill the fields');
+        }
+
+        addClient();
+
+        setName('');
+        setEmail('');
+        setPhone('');
+    }
 
     return (
         <>
@@ -30,7 +61,7 @@ const AddClientModal = () => {
                             </button>
                         </div>
                         <div className="modal-body">
-                            <form>
+                            <form onSubmit={(e) => handleSubmit(e)}>
                                 <div className="mb-3">
                                     <label htmlFor="name" className="form-label">Name</label>
                                     <input type="text" className="form-control" id="name" value={name} onChange={(e) => setName(e.target.value)} />
@@ -40,12 +71,12 @@ const AddClientModal = () => {
                                     <label htmlFor="email" className="form-label">Email</label>
                                     <input type="text" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                                 </div>
-                                
+
                                 <div className="mb-3">
                                     <label htmlFor="phone" className="form-label">Phone</label>
                                     <input type="text" className="form-control" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
                                 </div>
-                                
+
                                 <button className="btn btn-secondary">Submit</button>
                             </form>
                         </div>

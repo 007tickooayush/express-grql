0180cc9a1e3771bc2311d0a4d3dd9af7216e7b79
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-01-25 15:55:06 +0530

    design corrections

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -13,11 +13,10 @@ const ProjectCard = ({ project }: ProjectCardProps) => {
         <div className='col-md-6'>
             <div className="card mb-3">
                 <div className="card-body">
-                    <div className="d-flex justfy-content-between align-items-center">
+                    <div className="d-flex justify-content-between align-items-center">
                         <h5 className="card-title">
                             {project.name}
                         </h5>
-
                         <a href={`/projects/${project.id}`} className="btn btn-light">View</a>
                     </div>
                     <p className="small">
